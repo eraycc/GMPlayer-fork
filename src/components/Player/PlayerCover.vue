5fb77d17cd390c5da0d5cd6ea60fb113773e14e5
Burial0268 <burial0268@foxmail.com>
2025-03-30 03:17:16 +0800

    Revert "refactor: 沉浸式播放控件"
    This reverts commit 74727587dc3ffd4b9a4aff81c93e3dff355cf572.

--- src/components/Player/PlayerCover.vue
+++ src/components/Player/PlayerCover.vue
@@ -13,23 +13,106 @@
           '?param=1024y1024'
           : '/images/pic/default.png'
           " alt="shadow" />
-
-        <!-- Hover Metadata -->
-        <div class="metadata-overlay">
-          <div class="song-info">
-            <div class="title text-hidden">
-              {{ music.getPlaySongData ? music.getPlaySongData.name : $t("other.noSong") }}
-              <span v-if="music.getPlaySongData && music.getPlaySongData.alia">
-                {{ music.getPlaySongData.alia[0] }}
-              </span>
-            </div>
-            <div class="artist text-hidden" v-if="music.getPlaySongData && music.getPlaySongData.artist">
-              <AllArtists :artistsData="music.getPlaySongData.artist" />
-            </div>
-          </div>
-        </div>
       </div>
     </Transition>
+    <div class="control">
+      <div class="data">
+        <div class="desc">
+          <span class="name text-hidden">
+            {{
+              music.getPlaySongData
+                ? music.getPlaySongData.name
+                : $t("other.noSong")
+            }}
+          </span>
+          <div v-if="music.getPlaySongData" class="message">
+            <AllArtists :artistsData="[
+              music.getPlaySongData.artist[0],
+              music.getPlaySongData.artist[1]
+                ? music.getPlaySongData.artist[1]
+                : null,
+            ]" />
+            <span class="ablum text-hidden" @click="
+              routerJump('/album', {
+                id: music.getPlaySongData
+                  ? music.getPlaySongData.album.id
+                  : null,
+              })
+              ">
+              {{ music.getPlaySongData.album.name }}
+            </span>
+          </div>
+        </div>
+        <n-icon v-if="music.getPlaySongData && user.userLogin" class="like" size="20" :component="music.getSongIsLike(music.getPlaySongData.id)
+          ? FavoriteRound
+          : FavoriteBorderRound
+          " @click.stop="
+            music.getSongIsLike(music.getPlaySongData.id)
+              ? music.changeLikeList(music.getPlaySongData.id, false)
+              : music.changeLikeList(music.getPlaySongData.id, true)
+            " />
+      </div>
+      <div class="time">
+        <span>{{ music.getPlaySongTime.songTimePlayed }}</span>
+        <vue-slider v-model="music.getPlaySongTime.barMoveDistance" @drag-start="music.setPlayState(false)"
+          @drag-end="sliderDragEnd" @click.stop="
+            songTimeSliderUpdate(music.getPlaySongTime.barMoveDistance)
+            " :tooltip="'none'" />
+        <span>{{ music.getPlaySongTime.songTimeDuration }}</span>
+      </div>
+      <div class="buttons">
+        <n-icon :style="music.getPersonalFmMode
+          ? 'opacity: 0.2;pointer-events: none;'
+          : null
+          " size="16" class="mode" :component="music.getPlaySongMode === 'normal'
+            ? PlayCycle
+            : music.getPlaySongMode === 'random'
+              ? ShuffleOne
+              : PlayOnce
+            " @click="music.setPlaySongMode()" />
+        <n-icon v-if="!music.getPersonalFmMode" class="prev" size="30" :component="SkipPreviousRound"
+          @click.stop="music.setPlaySongIndex('prev')" />
+        <n-icon v-else class="dislike" size="20" :style="!user.userLogin ? 'opacity: 0.2;pointer-events: none;' : null"
+          :component="ThumbDownRound" @click="music.setFmDislike(music.getPersonalFmData.id)" />
+        <div class="play-state">
+          <n-button :loading="music.getLoadingState" secondary circle :keyboard="false" :focusable="false">
+            <template #icon>
+              <Transition name="fade" mode="out-in">
+                <n-icon size="42" :component="music.getPlayState ? PauseRound : PlayArrowRound"
+                  @click.stop="music.setPlayState(!music.getPlayState)" />
+              </Transition>
+            </template>
+          </n-button>
+        </div>
+        <n-icon class="next" size="30" :component="SkipNextRound" @click.stop="music.setPlaySongIndex('next')" />
+        <n-icon class="comment" size="18" :component="MessageFilled" @click="
+          routerJump('/comment', {
+            id: music.getPlaySongData ? music.getPlaySongData.id : null,
+          })
+          " />
+      </div>
+      <div class="volume">
+        <n-popover trigger="hover" placement="top-start" :keep-alive-on-hover="false">
+          <template #trigger>
+            <n-icon size="28" :component="persistData.playVolume == 0
+              ? VolumeOffRound
+              : persistData.playVolume < 0.4
+                ? VolumeMuteRound
+                : persistData.playVolume < 0.7
+                  ? VolumeDownRound
+                  : VolumeUpRound
+              " @click="volumeMute" />
+          </template>
+          {{
+            persistData.playVolume > 0
+              ? $t("general.name.mute")
+              : $t("general.name.unmute")
+          }}
+        </n-popover>
+        <vue-slider :tooltip="'none'" :min="0" :max="1" :interval="0.001" v-model="persistData.playVolume" />
+        <span>100%</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -113,6 +196,7 @@ const volumeMute = () => {
       filter: opacity(0.2) grayscale(0.8);
     }
 
+    // overflow: hidden;
     @media (max-width: 1200px) {
       width: 44vh;
       height: 44vh;
@@ -143,35 +227,212 @@ const volumeMute = () => {
     }
   }
 
-  .metadata-overlay {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    padding: 1.5rem;
-    background: rgba(var(--main-cover-color), 0.14);
-    backdrop-filter: blur(10px);
-    opacity: 0;
-    transition: opacity 0.3s ease;
-    color: rgb(var(--main-cover-color));
-    z-index: 2;
+  .control {
+    margin-top: 24px;
 
-    .song-info {
-      .title {
-        font-size: 1.25rem;
-        font-weight: 600;
-        margin-bottom: 0.5rem;
+    .data {
+      width: 50vh;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      @media (max-width: 1200px) {
+        width: 44vh;
       }
 
-      .artist {
-        font-size: 1rem;
-        opacity: 0.8;
+      @media (max-width: 870px) {
+        width: 40vh;
+      }
+
+      .desc {
+        width: 100%;
+        padding-right: 4px;
+
+        .name {
+          font-size: 23px;
+          font-weight: bold;
+          -webkit-line-clamp: 2;
+          line-clamp: 2;
+
+          @media (max-width: 1200px) {
+            font-size: 20px;
+          }
+        }
+
+        .message {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          margin: 2px 0 6px;
+          font-size: 2vh;
+          font-size: 15px;
+          width: 100%;
+          color: var(--main-cover-color);
+
+          @media (max-width: 1200px) {
+            font-size: 14px;
+          }
+
+          .ablum {
+            transition: all 0.3s;
+
+            &:hover {
+              color: #fff;
+            }
+
+            &::before {
+              content: "·";
+              margin: 0 4px;
+            }
+          }
+
+          .artists {
+            flex-wrap: nowrap;
+
+            :deep(.artist) {
+              display: inline-block;
+              white-space: nowrap;
+
+              .name {
+                color: var(--main-cover-color);
+
+                &:hover {
+                  color: #fff;
+                }
+              }
+            }
+          }
+        }
+      }
+
+      .like {
+        padding: 8px;
+        border-radius: 8px;
+        transition: all 0.3s;
+        cursor: pointer;
+
+        &:hover {
+          background-color: #ffffff26;
+        }
+
+        &:active {
+          transform: scale(0.9);
+        }
       }
     }
-  }
 
-  &:hover .metadata-overlay {
-    opacity: 1;
+    .volume,
+    .time {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      span {
+        opacity: 0.8;
+      }
+
+      .vue-slider {
+        margin: 0 10px;
+        width: 100% !important;
+        transform: translateY(-1px);
+        cursor: pointer;
+
+        :deep(.vue-slider-rail) {
+          background-color: #ffffff20;
+          border-radius: 25px;
+
+          .vue-slider-process {
+            background-color: var(--main-cover-color);
+          }
+
+          .vue-slider-dot {
+            width: 12px !important;
+            height: 12px !important;
+            box-shadow: none;
+          }
+
+          .vue-slider-dot-handle-focus {
+            box-shadow: none;
+          }
+
+          .vue-slider-dot-tooltip-inner {
+            background-color: var(--main-cover-color);
+            backdrop-filter: blur(2px);
+            border: none
+          }
+
+          .vue-slider-dot-tooltip-text {
+            color: var(--main-cover-color);
+          }
+
+          .vue-slider-dot-handle {
+            background-color: var(--main-cover-color);
+            backdrop-filter: blur(2px);
+          }
+        }
+      }
+    }
+
+    .buttons {
+      margin-top: 26px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .play-state {
+        --n-width: 42px;
+        --n-height: 42px;
+        color: var(--main-cover-color);
+        margin: 0 12px;
+        transition:
+          background-color 0.3s,
+          transform 0.3s;
+
+        .n-icon {
+          transition: opacity 0.1s ease-in-out;
+          color: var(--main-cover-color);
+        }
+
+        &:hover {
+          transform: scale(1.1);
+        }
+
+        &:active {
+          transform: scale(1);
+        }
+      }
+
+      .mode,
+      .comment {
+        &.n-icon {
+          opacity: 0.8;
+          margin: 0 6px;
+          padding: 8px;
+        }
+      }
+
+      .dislike {
+        padding: 10px !important;
+      }
+
+      .n-icon {
+        margin: 0 4px;
+        cursor: pointer;
+        padding: 6px;
+        border-radius: 8px;
+        transition: all 0.3s;
+
+        &:hover {
+          background-color: #ffffff4d;
+        }
+
+        &:active {
+          transform: scale(0.9);
+        }
+      }
+    }
   }
 }
 </style>
